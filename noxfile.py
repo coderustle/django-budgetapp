6280911b6ed8ef210ea7ccf7d53f83081bc7b83d
Madalin Popa <github@madalintech.com>
2022-06-29 15:16:19 +0300

    add commands for running tests

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@
 Automation tasks.
 """
 import nox
+from nox.sessions import Session
 
 SOURCE_CODE = "./budgetapp"
 
@@ -15,14 +16,14 @@ nox.options.default_venv_backend = "none"
 # FORMAT AND LINTING CODE TASKS
 # ----------------------------------------------------
 @nox.session(name="format")
-def format_code(session):
+def format_code(session: Session):
     """Task to format all files"""
     session.run("isort", ".")
     session.run("black", ".")
 
 
 @nox.session(name="lint")
-def lint_code(session):
+def lint_code(session: Session):
     """This taks is used to run linting on code"""
     session.run(
         "pylint",
@@ -31,4 +32,37 @@ def lint_code(session):
         "-f",
         "colorized",
         SOURCE_CODE,
+    )
+
+
+#
+# RUN UNIT TESTS IN DOCKER
+# ----------------------------------------------------
+@nox.session(name="test-unit")
+def test_unit(session: Session):
+    """Run all the unit tests in docker container"""
+    session.run(
+        "docker",
+        "compose",
+        "run",
+        "webapp-budgetapp",
+        "python",
+        "manage.py",
+        "test",
+        "tests.unit",
+        external=True,
+    )
+
+
+#
+# RUN FUNCTIONAL TESTS
+# ----------------------------------------------------
+@nox.session(name="test-functional")
+def test_functional(session: Session):
+    """Run all functional tests locally"""
+    session.run(
+        "python",
+        "manage.py",
+        "test",
+        "tests.functional",
     )

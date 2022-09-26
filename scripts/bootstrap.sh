16115b5cbe73b7bb0b05373ee7dadbb86d315fd2
Madalin Popa <contact@madalinpopa.com>
2022-09-26 19:35:13 +0000

    refactor: Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,22 +4,22 @@
 set -e
 
 if [ ! -f ".env" ]; then
-
+    
     # Create the .env file
     touch .env
-
+    
     echo "# Django variables"  >>.env
     echo "SECRET_KEY=\"$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-50} | head -n 1)\"" >>.env
-    echo 'DJANGO_SETTINGS_MODULE="budgetapp.settings.dev"' >>.env
+    echo 'DJANGO_SETTINGS_MODULE="budgetapp.settings.development"' >>.env
     echo 'DJANGO_DEBUG="True"' >>.env
     echo " " >>.env
     echo "# Database variables"  >>.env
     echo 'DEV_DB_NAME="budgetapp_dev"' >>.env
-    echo 'DEV_DB_HOST="db"' >>.env
+    echo 'DEV_DB_HOST="db-budgetapp"' >>.env
     echo 'DEV_DB_PASS="abc123"' >>.env
     echo 'DEV_DB_USER="budget"' >>.env
     echo " " >>.env
-
+    
 fi
 
 # =========================================

86c0136e1fb5849f43e0adf222aa90f23b25bf77
Madalin Popa <contact@madalinpopa.com>
2022-09-26 19:54:20 +0000

    feature: Add github workflow

---
+++ budgetapp/settings/dev.py
@@ -0,0 +1,50 @@
+"""
+Django settings for budgetapp project.
+"""
+
+# pylint: disable=wildcard-import unused-wildcard-import
+
+from .base import *
+
+# GENERAL
+# -----------------------------------------------------------------------------
+DEBUG = True
+
+# DATABASES
+# -----------------------------------------------------------------------------
+# Database settings
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DEV_DB_NAME"),
+        "USER": os.getenv("DEV_DB_USER"),
+        "PASSWORD": os.getenv("DEV_DB_PASS"),
+        "HOST": os.getenv("DEV_DB_HOST"),
+        "PORT": "5432",
+    }
+}
+
+# TEMPLATES
+# -----------------------------------------------------------------------------
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "budgetapp/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "debug": True,
+        },
+    },
+]
+
+# For tests to speed up
+# -----------------------------------------------------------------------------
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]

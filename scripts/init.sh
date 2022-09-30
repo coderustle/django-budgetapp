e72eb68b632280b73d833b1444a4bf32993e5509
Madalin Popa <contact@madalinpopa.com>
2022-09-30 06:04:42 +0000

    chore: Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ set -e
 # =========================================
 # Get env vars in the Dockerfile to show up in the SSH session
 # =========================================
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> ~/.profile)
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 # =========================================
 # Start SSH Server

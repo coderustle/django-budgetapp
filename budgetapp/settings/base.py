8f5ff82dd741f372de69265506eb798c0ee42147
Madalin Popa <contact@madalinpopa.com>
2022-09-30 07:37:49 +0000

    chore: Update base settings, add whitenoise static

--- budgetapp/settings/base.py
+++ budgetapp/settings/base.py
@@ -39,6 +39,7 @@ DJANGO_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
 ]
 

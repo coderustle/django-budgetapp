84c59f6c33a3576c4a52c7129882cb3292a5ec58
Madalin Popa <contact@madalinpopa.com>
2022-11-15 18:50:08 +0000

    chore: Update bootstrap env var

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,16 +10,16 @@ if [ ! -f ".env" ]; then
     
     echo 'BUILD_ENV="local"' >>.env
     echo " " >>.env
+    
     echo "# Django variables"  >>.env
     echo "SECRET_KEY=\"$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-50} | head -n 1)\"" >>.env
-    
     echo 'DJANGO_SETTINGS_MODULE="budgetapp.settings.dev"' >>.env
     echo 'DJANGO_DEBUG="True"' >>.env
     
     echo " " >>.env
     echo "# Database variables"  >>.env
     echo 'DB_NAME="budgetapp_dev"' >>.env
-    echo 'DB_HOST="db-budgetapp"' >>.env
+    echo 'DB_HOST="localhost"' >>.env
     echo 'DB_PASS="abc123"' >>.env
     echo 'DB_USER="budget"' >>.env
     echo " " >>.env

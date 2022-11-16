9eae6a28da46a35a6c3da47a4a992d108a685148
Madalin Popa <contact@madalinpopa.com>
2022-11-16 22:39:57 +0200

    chore: Format code

--- budgetapp/applications/budgets/models.py
+++ budgetapp/applications/budgets/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Budget(models.Model):
 
+class Budget(models.Model):
     name = models.CharField(null=False, max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    
     class Meta:
         ordering = ("-created",)
         indexes = [

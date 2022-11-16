f806ff5b2b9df49f10fdb05b75777ec5c8ed9906
Madalin Popa <contact@madalinpopa.com>
2022-11-16 22:23:33 +0200

    chore: Update apps config

--- budgetapp/applications/budgets/apps.py
+++ budgetapp/applications/budgets/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BudgetsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'budgets'
+    name = 'budgetapp.applications.budgets'

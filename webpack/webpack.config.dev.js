589c89163c24a05b6dd1c4439d0b9106fd527ad3
Madalin Popa <contact@madalinpopa.com>
2022-11-17 19:49:50 +0200

    wip/budgets

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,7 +2,7 @@
  * Webpack modules
  */
 const { merge } = require('webpack-merge');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /**
  * Common configuration
  */
@@ -17,4 +17,26 @@ module.exports = merge(common, {
    * Build and rebuild fast.
    */
   devtool: 'eval',
+  /**
+   * OUTPUT
+   *
+   * The output property tells webpack where to emit the bundles it creates
+   * and how to name these files. All the files are outputed to ../project_name/static/js/
+   */
+  output: {
+    filename: '[name].js'
+  },
+  /**
+   * PLUGINS
+   *
+   * While loaders are used to transform certain types of modules, plugins can be
+   * leveraged to perform a wider range of tasks like bundle optimization,
+   * asset management and injection of environment variables
+   */
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ]
+
 });

bde83c4894f36170c3ec81e5b05a5981578225cd
Madalin Popa <contact@madalinpopa.com>
2022-11-17 19:03:44 +0200

    wip/budgets

--- budgetapp/applications/budgets/views.py
+++ budgetapp/applications/budgets/views.py
@@ -6,10 +6,11 @@ from django.views.decorators.http import require_GET
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 
+
 @require_GET
 def home_page(request: HttpRequest) -> HttpResponse:
     """
     Budgets home page
     """
-    template = "index.html"
+    template = "budgets/home.html"
     return TemplateResponse(request, template)
--- budgetapp/settings/base.py
+++ budgetapp/settings/base.py
@@ -48,6 +48,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     "webpack_loader",
+    "djmoney",
 ]
 
 LOCAL_APPS = [
@@ -118,19 +119,19 @@ LOGOUT_REDIRECT_URL = "/users/login/"
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".UserAttributeSimilarityValidator",
+        ".UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".MinimumLengthValidator",
+        ".MinimumLengthValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".CommonPasswordValidator",
+        ".CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".NumericPasswordValidator",
+        ".NumericPasswordValidator",
     },
 ]
 

981f90ff9e71af5005d2a8c2dad65ab591e77394
Madalin Popa <contact@madalinpopa.com>
2022-11-24 23:46:24 +0000

    chore: Refactor index.html page

--- budgetapp/applications/users/urls.py
+++ budgetapp/applications/users/urls.py
@@ -4,6 +4,8 @@ urls.py
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from . import views
+
 app_name = "users"
 
 urlpatterns = [
@@ -16,5 +18,10 @@ urlpatterns = [
         "logout/",
         view=LogoutView.as_view(),
         name="logout",
+    ),
+    path(
+        "register",
+        view=views.register_page,
+        name="register",
     ),
 ]
--- budgetapp/applications/users/views.py
+++ budgetapp/applications/users/views.py
@@ -1,3 +1,20 @@
 """
 Django views
 """
+from django.template.response import TemplateResponse
+from django.views.decorators.http import require_http_methods
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+from .forms import NewUserForm
+
+
+@require_http_methods(["GET", "POST"])
+def register_page(request: HttpRequest) -> HttpResponse:
+    """Render register page and create new user"""
+    template = "registration/register.html"
+
+    if request.method == "GET":
+        form = NewUserForm()
+        context = {"form": form}
+        return TemplateResponse(request, template=template, context=context)

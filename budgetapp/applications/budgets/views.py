ff42c2f5ff5b90216289338fc4157ad5c1eccb3a
Madalin Popa <contact@madalinpopa.com>
2022-11-24 22:30:26 +0000

    refactor: Update users views.py

--- budgetapp/applications/budgets/views.py
+++ budgetapp/applications/budgets/views.py
@@ -5,8 +5,10 @@ from django.template.response import TemplateResponse
 from django.views.decorators.http import require_GET
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 @require_GET
 def home_page(request: HttpRequest) -> HttpResponse:
     """
--- budgetapp/applications/users/views.py
+++ budgetapp/applications/users/views.py
@@ -1,21 +1,3 @@
 """
 Django views
 """
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
-
-app_name = "users"
-
-
-urlpatterns = [
-    path(
-        "login/",
-        view=LoginView.as_view(),
-        name="login",
-    ),
-    path(
-        "logout/",
-        view=LogoutView.as_view(),
-        name="logout",
-    ),
-]

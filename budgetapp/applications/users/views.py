a81849e17cc6a6849955713ac34633b53d7857e3
Madalin Popa <contact@madalinpopa.com>
2022-11-28 18:38:09 +0000

    chore: Finish register

--- budgetapp/applications/users/views.py
+++ budgetapp/applications/users/views.py
@@ -20,6 +20,11 @@ def register_request(request: HttpRequest) -> HttpResponse:
     """Render register page and create new user"""
     template = "registration/register.html"
 
+    if request.method == "GET":
+        form = NewUserForm()
+        context = {"form": form}
+        return TemplateResponse(request, template=template, context=context)
+
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
@@ -31,15 +36,19 @@ def register_request(request: HttpRequest) -> HttpResponse:
             request, "Unsuccessful registration. Invalid information."
         )
         print(form.errors)
-    form = NewUserForm()
-    context = {"form": form}
-    return TemplateResponse(request, template=template, context=context)
+        context = {"form": form}
+        return TemplateResponse(request, template=template, context=context)
 
 
 @require_http_methods(["GET", "POST"])
 def login_request(request: HttpRequest) -> HttpResponse:
     """Render the login page and authenticates the user"""
     template = "registration/login.html"
+
+    if request.method == "GET":
+        form = AuthenticationForm()
+        context = {"form": form}
+        return TemplateResponse(request, template=template, context=context)
 
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -63,6 +72,3 @@ def login_request(request: HttpRequest) -> HttpResponse:
             return HttpResponseHXRedirect(
                 redirect_to=reverse_lazy("users:login")
             )
-    form = AuthenticationForm()
-    context = {"form": form}
-    return TemplateResponse(request, template=template, context=context)

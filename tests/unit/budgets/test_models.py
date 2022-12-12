b53ea41d34168746c50a834b7f78c4d5b0a58fcf
Madalin Popa <contact@madalinpopa.com>
2022-12-12 18:34:50 +0000

    chore: Add some more tests

--- tests/unit/budgets/test_models.py
+++ tests/unit/budgets/test_models.py
@@ -4,6 +4,9 @@ test_models.py
 from django.test import TestCase
 from tests import fixtures
 
+from budgetapp.applications.budgets.models import Budget
+from guardian.shortcuts import assign_perm, get_objects_for_user
+
 
 class TestBase(TestCase):
     """Base test class containing setup for all budgets models"""
@@ -16,3 +19,39 @@ class TestBase(TestCase):
         super().setUpTestData()
         cls.user1 = fixtures.create_user()
         cls.user2 = fixtures.create_user()
+
+
+class TestBudgetModel(TestBase):
+    """Contains the tests for Budget model"""
+
+    def test_create_new_budget(self):
+        """Test creating new budget"""
+        budget = Budget.objects.create(name="Test Budget")
+        self.assertEqual(budget.name, "Test Budget")
+
+    def test_budget_str(self):
+        """Test budget string representation"""
+        budget = Budget.objects.create(name="Test Budget")
+        self.assertEqual(str(budget), "Test Budget")
+
+    def test_budget_permission_forbiden(self):
+        """Test the permission for a budget"""
+        budget = Budget.objects.create(name="Test Budget")
+        self.assertFalse(self.user1.has_perm("budgets.view_budget", budget))
+
+    def test_budget_permission_allowed(self):
+        """Test the permission for a budget"""
+        budget = Budget.objects.create(name="Test Budget")
+        assign_perm("budgets.view_budget", self.user1, budget)
+        self.assertTrue(self.user1.has_perm("budgets.view_budget", budget))
+
+    def test_budget_permission_allowed_for_user(self):
+        """Test the permission for a budget"""
+        budget1 = Budget.objects.create(name="Test Budget")
+        budget2 = Budget.objects.create(name="Test Budget 2")
+        assign_perm("budgets.view_budget", self.user1, budget1)
+        assign_perm("budgets.view_budget", self.user1, budget2)
+
+        # all budgets for user1
+        budgets = get_objects_for_user(self.user1, "budgets.view_budget")
+        self.assertEqual(len(budgets), 2)

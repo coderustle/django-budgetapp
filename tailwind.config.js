4f184a0bfa7b561bbc5023a55d62188e19a96a86
Madalin Popa <contact@madalinpopa.com>
2023-05-21 16:31:03 +0300

    Update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
             container: {
                 center: true,
             },
+            colors: {
+                'primary': '#FCBE5B',
+                'secondary': '#F5604A',
+                'third': '#7EB67E',
+                'dark': '#202020'
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif']
+            }
         },
     },
     plugins: [],

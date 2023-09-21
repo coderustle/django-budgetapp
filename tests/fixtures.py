dbd8995d6cfbb6a3abc770d95ddb0c8969c55ed8
Madalin Popa <contact@madalinpopa.com>
2023-09-21 20:09:56 +0300

    Update test fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,12 +9,15 @@ from faker import Faker
 from budgetapp.apps.users.models import User
 
 
-def create_user() -> User:
+def create_user(
+    username: str | None = None,
+    password: str | None = None,
+) -> User:
     """Create a new user in database"""
     fake = Faker("ro_Ro")
     user = get_user_model().objects.create(
-        username=fake.user_name(),
-        password=fake.text(max_nb_chars=10),
+        username=username if username else fake.user_name(),
+        password=password if password else fake.text(max_nb_chars=10),
         email=fake.email(),
     )
     user.save()

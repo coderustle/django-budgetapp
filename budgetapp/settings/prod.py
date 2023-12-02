a6b02079bf8c76375023fc1deb1393453b181d56
Madalin Popa <contact@madalinpopa.com>
2023-12-02 17:27:29 +0200

    Fix postgres password

--- budgetapp/settings/prod.py
+++ budgetapp/settings/prod.py
@@ -32,7 +32,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": "5432",
     }
